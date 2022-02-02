3323bd306fbc7ae5e810f5f571d146465f34626a
aditya-mitra <55396651+aditya-mitra@users.noreply.github.com>
2022-02-02 10:15:36 +0530

    get page details

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import superAgent from 'superagent';
 import cheerio from 'cheerio';
 
 const BASE_URL = 'https://stackoverflow.com';
+const PAGE_SIZE = 50;
 
-async function getUrlsFromPage(pageNo: number): Promise<string[]> {
+interface PageDetail {
+	url: string;
+	upvotes: number;
+	answers: number;
+}
+
+async function getDetailsFromPage(pageNo: number): Promise<PageDetail[]> {
 	const dom = await superAgent
-		.get(`${BASE_URL}/questions?page=${pageNo}`)
+		.get(`${BASE_URL}/questions`)
+		.query({ page: pageNo, pagesize: PAGE_SIZE })
 		.then((response) => response.text)
 		.catch((err: superAgent.ResponseError) => {
 			console.log('encountered error ', err.message);
@@ -13,24 +21,49 @@ async function getUrlsFromPage(pageNo: number): Promise<string[]> {
 		});
 
 	if (!dom) {
-		return [];
+		return []; // throw error here instead of return
 	}
 
 	const $ = cheerio.load(dom);
-	const urls: string[] = [];
+	const pageDetails: PageDetail[] = [];
 
-	$('.question-summary .summary h3 a').each((_i, el) => {
-		urls.push(el.attribs['href']);
+	$('.question-summary').each((_i, el) => {
+		const url = $(el).find('.summary h3 a').attr('href') ?? '';
+		const upvotes = parseInt(
+			$(el).find('.stats .vote .votes strong').text()
+		);
+		const answers = parseInt($(el).find('.stats .status strong').text());
+
+		pageDetails.push({
+			url,
+			upvotes,
+			answers,
+		});
 	});
 
-	return urls;
+	return pageDetails;
 }
 
-async function getDataFromUrl(url: string) {
-    
+async function storeDetailsInDatabase(pageDetails: PageDetail[]) {}
+
+async function main() {
+	for (let i = 0; i <= 1; ++i) {
+		// change to while(true)
+
+		const pageDetailPromises: Promise<PageDetail[]>[] = [];
+
+		for (let pageNo = i * 5 + 1; pageNo <= (i + 1) * 5; ++pageNo) {
+			pageDetailPromises.push(getDetailsFromPage(pageNo));
+		}
+
+		const pageDetails = await Promise.all(pageDetailPromises);
+		console.log(pageDetails);
+	}
 }
 
-async function addUrlsToDatabase(urls: string[]) {
+main();
+
+/*async function addUrlsToDatabase(urls: string[]) {
 	for (let i = 0; i < urls.length; ++i) {
 		const group: Promise<void>[] = [];
 		for (let j = 0; j < urls.length && j < 5; ++j) {
@@ -38,14 +71,6 @@ async function addUrlsToDatabase(urls: string[]) {
 			group.push(getDataFromUrl(urls[i + j]));
 		}
 
-        await Promise.all(group);
+		await Promise.all(group);
 	}
-}
-
-async function main() {
-	for (let pageNo = 1; pageNo <= 1; ++pageNo) {
-		const urls = await getUrlsFromPage(pageNo);
-	}
-}
-
-main();
+}*/
